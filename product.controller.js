a65ca3f0ab90a8fcacb9ec798ec3630d9a288c60
alyaa-awny <101072088+alyaa-awny@users.noreply.github.com>
2022-08-01 02:22:28 +0200

    Add files via upload

---
+++ product.controller.js
@@ -0,0 +1,16 @@
+const productModel = require("../../db/models/user.model")
+const {resGenerator} = require("../../helper/methods")
+
+class Product{
+
+
+
+
+
+    
+
+}
+
+
+
+module.exports  = Product
---
+++ product.rout.js
@@ -0,0 +1,7 @@
+const router = require ("express").Router()
+const user = require("../app/controllers/product.controller")
+
+// router.post("/register", user.register)
+
+
+module.exports = router 
