cdb870f8c215f68837ab7437c797b932563080f2
alyaa-awny <101072088+alyaa-awny@users.noreply.github.com>
2022-08-01 02:26:45 +0200

    Delete product.rout.js

--- product.rout.js
+++
@@ -1,7 +0,0 @@
-const router = require ("express").Router()
-const user = require("../app/controllers/product.controller")
-
-// router.post("/register", user.register)
-
-
-module.exports = router 
